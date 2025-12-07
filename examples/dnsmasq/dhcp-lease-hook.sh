007cd19a05771a173c01cdb17c463ea81f109bd7
Ben <andreabenini@users.noreply.github.com>
2025-12-08 00:36:52 +0100

    Adding custom delays per host --- - On some hosts like Firestick 4k Max booting time and CPU are fast enough - On slower devices like Firestick 4k (1st or 2nd gen) it might require   a little bit more. Same goes if you're attaching external drives or   additional peripherals to it.

--- examples/dnsmasq/dhcp-lease-hook.sh
+++ examples/dnsmasq/dhcp-lease-hook.sh
@@ -22,9 +22,22 @@ ACTION=$1
 MAC=$2
 IP=$3
 HOSTNAME=$4
+declare -A firestick_delay
 
-# Firestick devices (array)
+
+# Configuration Part
+# ----------------------------------------------------
+# Firestick devices
 firestick_devices=("device1" "device2" "device3")
+# default delay for all devices
+firestick_delayDefault=15
+# custom delay defined per single host, if needed (like because it's a slow/old one)
+firestick_delay_device2=20
+
+
+
+# business logic, do not touch anything below
+# ----------------------------------------------------
 
 # Required utilities to execute this script
 required_tools=("adb")
@@ -32,6 +45,7 @@ required_tools=("adb")
 # Log all events to a dedicated file
 LOG_FILE="/tmp/dnsmasq_leases.log"
 TIMESTAMP=$(date)
+
 # Initial requirements checks
 for tool in "${required_tools[@]}"; do
     if [ "$(which $tool 2>/dev/null)" == "" ]; then
@@ -42,16 +56,23 @@ done
 # Log everything
 echo "> $TIMESTAMP  [$ACTION]  $MAC -> $IP [$HOSTNAME]" >> $LOG_FILE
 
-# searching firestick devices
+# working on firestick devices
 for host in "${firestick_devices[@]}"; do
     if [ "$host" == "$HOSTNAME" ] && [ "$ACTION" != "del" ]; then
         # [add] renewed lease, [old] updated lease.  Sideloading launcher injection
-        sleep 15
-        echo ">                                      Firestick TV detected, hacking it with custom launcher" >> $LOG_FILE
+        eval "host_delay=\"\$firestick_delay_${host}\""
+        if [ "$host_delay" = "" ]; then
+            host_delay=$firestick_delayDefault
+        fi
+        echo -n ">                                      Firestick TV detected, hacking it with custom launcher (delay:$host_delay)" >> $LOG_FILE
+        sleep $host_delay
+        echo " [started]" >> $LOG_FILE
         adb connect $host >/dev/null
         adb shell "nohup sh /data/local/tmp/backfire.sh >/dev/null 2>&1 &"
     fi
 done
 # Prune log file to keep just last 100 lines
 tail -n 100 $LOG_FILE > ${LOG_FILE}.tmp && mv ${LOG_FILE}.tmp $LOG_FILE
+
 exit 0
+
